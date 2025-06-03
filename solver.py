fb8addda996ea9d3589f5cb52a1557c8cab03a23
Junyoung Shin <jun021222@naver.com>
2025-06-04 02:57:00 +0900

    Bug fix and optimization

--- solver.py
+++ solver.py
@@ -5,8 +5,8 @@ def find_first_available_box_index(apples_arr):
 
     for i in range(h):
         for j in range(w):
-            for k in range(i, h):
-                for l in range(j, w):
+            for k in range(i, h+1):
+                for l in range(j, w+1):
                     box = apples_arr[i:k, j:l]
                     if box.sum() == 10: return (i, k, j, l)
                     if box.sum() > 10 : break
@@ -20,10 +20,13 @@ def greedy_solver(apples_box):
         box_index = find_first_available_box_index(apples_box)
         if box_index is None: break
 
-        solution.append(box_index)
         i, k, j, l = box_index
+        # optimize move: skip zero area
+        _box = apples_box[i:k, j:l]
+        nonzero_i, nonzero_j = _box.nonzero()
+        i, j = i+nonzero_i.min(), j+nonzero_j.min()
 
-
+        solution.append((i, k, j, l))
         score += (apples_box[i:k, j:l] != 0).sum() # increse score by number of removed apples
         apples_box[i:k, j:l] = 0 # fill selected area to 0
     
