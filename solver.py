7a11e62c6d6db9356446aeae3e18245d1b018c92
Junyoung Shin <jun021222@naver.com>
2025-06-10 15:48:00 +0900

    Refactor index optimization part

--- solver.py
+++ solver.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+def _optimize_index(apples_arr, index):
+    """
+    optimizing index by removing preceding zeros area.
+    """
+
+    i, k, j, l = index
+    
+    box = apples_arr[i:k, j:l]
+    nonzero_i, nonzero_j = box.nonzero()
+    i, j = i+nonzero_i.min(), j+nonzero_j.min()
+
+    return i, k, j, l
+
 def find_first_available_box_index(apples_arr):
     h, w = apples_arr.shape
 
@@ -8,8 +21,27 @@ def find_first_available_box_index(apples_arr):
             for k in range(i, h+1):
                 for l in range(j, w+1):
                     box = apples_arr[i:k, j:l]
-                    if box.sum() == 10: return (i, k, j, l)
+                    if box.sum() == 10: 
+                        optimized_index = _optimize_index(apples_arr, (i, k, j, l))
+                        return optimized_index
                     if box.sum() > 10 : break
+
+def find_all_available_box_indices(apples_arr):
+    h, w = apples_arr.shape
+    box_indices = []
+
+    for i in range(h):
+        for j in range(w):
+            for k in range(i, h+1):
+                for l in range(j, w+1):
+                    box = apples_arr[i:k, j:l]
+                    if box.sum() == 10: 
+                        optimized_index = _optimize_index(apples_arr, (i, k, j, l))
+                        if optimized_index not in box_indices:
+                            box_indices.append(optimized_index)
+                    if box.sum() > 10 : break
+    
+    return box_indices
 
 def greedy_solver(apples_arr):
     apples_arr = apples_arr.copy()
@@ -21,16 +53,19 @@ def greedy_solver(apples_arr):
         if box_index is None: break
 
         i, k, j, l = box_index
-        # optimize move: skip zero area
-        _box = apples_arr[i:k, j:l]
-        nonzero_i, nonzero_j = _box.nonzero()
-        i, j = i+nonzero_i.min(), j+nonzero_j.min()
 
         solution.append((i, k, j, l))
         score += (apples_arr[i:k, j:l] != 0).sum() # increse score by number of removed apples
+        
         apples_arr[i:k, j:l] = 0 # fill selected area to 0
     
     return dict(solution=solution, score=score)
+
+def dfs_solver(apples_arr):
+    def _dfs(apples_arr, solution=[], score=0, depth=0):
+        pass
+    
+    pass
 
 if __name__ == '__main__':
     test_case = np.loadtxt('./test_case/case_01.txt', dtype=np.uint8, delimiter=' ')
